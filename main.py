3818f735c12419873e78835bd04a9fa840e52af1
APaud3l <aamod.paudel@coderacademy.edu.au>
2025-09-20 09:51:39 +1000

    init: Initialise the necessary packages, getting the env values ready

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+from init import db
+import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+    print("Flask server started.")
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+    # or 
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI")
+
+    db.init_app(app)
+    return app
