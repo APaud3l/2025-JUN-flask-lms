2103e27fb6010f73820e3fac8da9833b28cc20b0
APaud3l <aamod.paudel@coderacademy.edu.au>
2025-09-22 19:53:01 +1000

    feat(Teacher): Created the model and added seed values to the cli_controller.py

--- controllers/cli_controller.py
+++ controllers/cli_controller.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from init import db
 from models.student import Student
+from models.teacher import Teacher
 
 db_commands = Blueprint("db", __name__)
 
@@ -17,16 +18,32 @@ def drop_tables():
 @db_commands.cli.command("seed")
 def seed_tables():
     # Create an instance of the Model
-    students = [Student(
-        name="Alice",
-        email="alice@email.com",
-        address="Syd"
-    ), Student(
-        name="Bob",
-        email="bob@email.com"
-    )]
-    # Add to the session
-    db.session.add_all(students)
+    # students = [Student(
+    #     name="Alice",
+    #     email="alice@email.com",
+    #     address="Syd"
+    # ), Student(
+    #     name="Bob",
+    #     email="bob@email.com"
+    # )]
+    # # Add to the session
+    # db.session.add_all(students)
+
+    teachers = [
+        Teacher(
+            name="Teacher 1",
+            department="Science",
+            address="Syd"
+        ), Teacher(
+            name="Teacher 2",
+            department="Management",
+            address="Bris"
+        )
+    ]
+
+    # Add the teachers info to the session
+    db.session.add_all(teachers)
+
     # Commit
     db.session.commit()
 
